6c625b7a2155ccacb9a9be3597dd08682eaa2fc7
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-15 14:32:14 +0600

    icons added in dashboard

--- src/pages/TeachersPages/Dashboard/DashboardSidebar/Sidebar.jsx
+++ src/pages/TeachersPages/Dashboard/DashboardSidebar/Sidebar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaPlus, FaExternalLinkAlt, FaPencilAlt, FaRegPaperPlane, FaUserFriends } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className="space-y-3">
+    <div className="space-y-5">
       <div className="side-nav">
-        <FaHome className="w-7 h-7"></FaHome>
+        <FaHome className="w-6 h-6"></FaHome>
         <NavLink className='text-base font-bold '>Overview</NavLink>
       </div>
       <div className="side-nav">
-        <FaHome className="w-7 h-7"></FaHome>
-        <NavLink className='text-base font-bold '>Overview</NavLink>
+        <FaPlus className="w-6 h-6"></FaPlus>
+        <NavLink className='text-base font-bold '>Add Course</NavLink>
       </div>
       <div className="side-nav">
-        <FaHome className="w-7 h-7"></FaHome>
-        <NavLink className='text-base font-bold '>Overview</NavLink>
+        <FaPencilAlt className="w-6 h-6"></FaPencilAlt>
+        <NavLink className='text-base font-bold '>Updates</NavLink>
       </div>
       <div className="side-nav">
-        <FaHome className="w-7 h-7"></FaHome>
-        <NavLink className='text-base font-bold '>Overview</NavLink>
+        <FaRegPaperPlane className="w-6 h-6"></FaRegPaperPlane>
+        <NavLink className='text-base font-bold '>Published Courses</NavLink>
       </div>
       <div className="side-nav">
-        <FaHome className="w-7 h-7"></FaHome>
-        <NavLink className='text-base font-bold '>Overview</NavLink>
+        <FaExternalLinkAlt className="w-6 h-6"></FaExternalLinkAlt>
+        <NavLink className='text-base font-bold '>Preview</NavLink>
+      </div>
+      <div className="side-nav">
+        <FaUserFriends className="w-6 h-6"></FaUserFriends>
+        <NavLink className='text-base font-bold '>Students</NavLink>
       </div>
     </div>
   );
